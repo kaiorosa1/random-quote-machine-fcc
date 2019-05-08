41782021886541585664268a21eb67bfb7827def
kaiorosa1 <kaiosrosa@gmail.com>
2019-05-08 12:29:14 -0300

    add finished functionality

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 // Start React App
 class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
       <div class="centered">
-        <Box initialQuote="Initial quote"/>
+        <Box initialQuote="Mulher Sofre" initialAuthor="Rosa, Karen"/>
       </div>
     );
   }
@@ -13,22 +16,35 @@ class Box extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      quote: props.initialQuote
+      quote: props.initialQuote,
+      author: props.initialAuthor
     }
   }
   
   changeQuote(){
-    
-    this.setState({
-      quote :"Another Quote"
-    });
+    const quotesList =[
+      {quote :"We'll never fall apart cuz we fit together right.",
+      author: "Lovato, Demi"},
+       {quote :"Paranoica",
+      author: "Rosa, Karen"},
+       {quote :"Like Neon lights",
+      author: "Lovato, Demi"},
+       {quote :"Love Yourself",
+      author: "BTS, Johnny"},
+       {quote :"Love is beautiful",
+      author: "LastName , First Name"}
+    ];
+    let randomIndex = Math.floor(Math.random()*quotesList.length);
+    this.setState(quotesList[randomIndex]);
     
   }
   render() {
     return (
-      <div>
-        <h1>{this.state.quote}</h1>
-        <button onClick={this.changeQuote.bind(this)}>select a quote</button>
+      <div id="quote-box">
+        <h1 id="text">"{this.state.quote}"</h1>
+        <h2 id="author">- {this.state.author}</h2>
+        <button><a id="tweet-quote" href="https://twitter.com/intent/tweet">tweet this quote</a></button>
+        <button id="new-quote" onClick={this.changeQuote.bind(this)}>new quote</button>
       </div>
     );
   }
